7efb8b143f76e27b657f962b86015159aa75f9bd
ryandemarigny <ryandemarigny@gmail.com>
2024-12-15 19:13:02 -0800

    added guessing game

---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Enter a guess!");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
